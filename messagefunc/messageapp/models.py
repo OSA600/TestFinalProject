33ccc5fd53d43a0e6a9395c924ce184ee76d9586
Tavia Chai Ling <tavialee42@gmail.com>
2022-11-09 14:51:12 +0100

    add super user

--- messagefunc/messageapp/models.py
+++ messagefunc/messageapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Employees(models.Model):
+    emp_id = models.ForeignKey(Employees, on_delete=models.PROTECT)
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    date_de_demarrage = models.DateTimeField
+    departement = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nom, self.prenom, self.departement, self.date_de_demarrage, self.emp_id
+

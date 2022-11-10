59f1015a9293ed4df0c02893d85e458e70532e93
Tavia Chai Ling <tavialee42@gmail.com>
2022-11-10 15:17:21 +0100

    DB

--- messagefunc/messageapp/models.py
+++ messagefunc/messageapp/models.py
@@ -1,17 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
+
+
 class Employees(models.Model):
-    nom = models.CharField(max_length=50)
-    prenom = models.CharField(max_length=50)
-    dateDeDemarrage = models.DateTimeField
-    departement = models.CharField(max_length=50)
+    nom = models.CharField(max_length=50, default=None)
+    prenom = models.CharField(max_length=50, default=None)
+    dateDeDemarrage = models.DateTimeField(default=timezone.now)
+    departement = models.CharField(max_length=50, default=None)
     lieu = models.CharField(max_length=50, default=None)
     natureDuPoste = models.CharField(max_length=50, default=None)
-    refAcces = models.CharField(max_length=50, default=None)
-    refOrdi = models.CharField(max_length=50, default=None)
-    refPhone = models.CharField(max_length=50, default=None)
+    refAcces = models.CharField(max_length=50, default=None, null=False, blank=False)
+    refOrdi = models.CharField(max_length=50, default=None, null=False, blank=False)
+    refPhone = models.CharField(max_length=50, default=None, null=False, blank=False)
 
     def __str__(self):
         return self.nom, self.prenom, self.departement, self.dateDeDemarrage, self.dateDeDemarrage, self.lieu, self.natureDuPoste, self.refOrdi, self.refPhone, self.refAcces
--- messagefunc/messagefunc/views.py
+++ messagefunc/messagefunc/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from datetime import datetime
 
 
 def index(request):
